67ef4770b06fb7f644d16e6ed73355c49109208f
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-03-24 10:10:33 +0000

    Player square

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BreakoutGame
 This is my version of the Breakout Game I am building using pyGames.
+
+I hope to replicate the breakout game shown below:
+
+<img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Breakout_game_screenshot.png/220px-Breakout_game_screenshot.png">
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((720, 900))
 clock = pygame.time.Clock()
 running = True
 
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("grey")
 
     # RENDER YOUR GAME HERE
+    # TODO: Creating a main player character
+    color = (0, 141, 218)
+    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 30))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
