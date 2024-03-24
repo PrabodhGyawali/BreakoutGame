e60b0186974d5e47cd71ceae938f92e6ed3ffce8
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-03-24 17:53:03 +0000

    Array of tiles with loop

--- main.py
+++ main.py
@@ -35,32 +35,23 @@ while running:
     wall_left = pygame.draw.rect(screen, BLUE, pygame.Rect(0, y*79.5, x*1, y*3))
     wall_right = pygame.draw.rect(screen, BLUE, pygame.Rect(x*62.2, y*79.5, x*1, y*3))
 
-    RED = (237,25,9)
-    ORANGE = (255,69,0)
-    GREEN = (50,205,50)
-    YELLOW = (255,255,0)
-
-    # TODO use loop to generate 14 red tiles with spacing of 1 px
-    red_tiles = []
+    # TODO use a loop to generate red tiles, green tiles, orange tiles, yellow tiles
+    RED, ORANGE, GREEN, YELLOW = (237,25,9), (242,133,0), (50,205,50), (255,255,0)
+    COLORS = (RED, ORANGE, GREEN, YELLOW)
+    red_tiles, orange_tiles, green_tiles, yellow_tiles = [], [], [], []
+    tile_array = [red_tiles, orange_tiles, green_tiles, yellow_tiles]
     # red tile has no spacing
-    red_tiles.append(pygame.draw.rect(screen, RED, pygame.Rect(x*1, y*20, x*4, y*1.5)))
-    for i in range(1, 14):
-        tile = pygame.draw.rect(screen, RED, pygame.Rect(x*1 + (i*x*4) + i*space, y*20, x*4, y*1.5))
-        red_tiles.append(tile)
-    # second row of red tiles
-    red_tiles.append(pygame.draw.rect(screen, RED, pygame.Rect(x*1, y*21.5 + space, x*4, y*1.5)))
-    for i in range(1, 14):
-        tile = pygame.draw.rect(screen, RED, pygame.Rect(x*1 + (i*x*4) + i*space, y*21.5 + space, x*4, y*1.5))
-        red_tiles.append(tile)
-    
-
-    
-    # 
-    # orange_tiles = []
-    # orange_tiles.append(pygame.draw.rect(screen, ORANGE, pygame.Rect(x*2, y*20 + space, x*6, y*3)))
-    # for i in range(1, 14):
-    #     tile = pygame.draw.rect(screen, RED, pygame.Rect(x*2 + (i*x*6) + i*space, y*20, x*6, y*3))
-    #     red_tiles.append(tile)
+    for j in range(4):    
+        for k in range(1,3):
+            for i in range(14):
+                tile_array[j].append(pygame.draw.rect(screen, COLORS[j], pygame.Rect(
+                    x*1 + (i*x*4) + i*space,
+                    y*20 + k*x*1.5 + k*space +2*j*x*1.5 + 2*j*space,
+                    x*4, 
+                    y*1.5)))
+                
+        
+        
 
 
     # flip() the display to put your work on screen
