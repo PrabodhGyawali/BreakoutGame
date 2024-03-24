af4a0cdeda8715b74e7cf13c36c556422554ebe5
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-03-24 10:30:00 +0000

    Walls

--- main.py
+++ main.py
@@ -13,12 +13,23 @@ while running:
             running = False
     
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("grey")
+    screen.fill("black")
 
     # RENDER YOUR GAME HERE
     # TODO: Creating a main player character
-    color = (0, 141, 218)
-    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 30))
+    BLUE = (0, 141, 218)
+    player = pygame.draw.rect(screen, BLUE, pygame.Rect(20, 800, 60, 30))
+
+    # TODO: Creating Game borders 
+    WHITE = (255, 255, 255)
+    wall_left = pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, 20, 900))
+    wall_right = pygame.draw.rect(screen, WHITE, pygame.Rect(700, 0, 20, 900))
+
+    # TODO: Add blue things onto the border
+    wall_left = pygame.draw.rect(screen, BLUE, pygame.Rect(0, 795, 20, 40))
+    wall_right = pygame.draw.rect(screen, BLUE, pygame.Rect(700, 795, 20, 40))
+
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
