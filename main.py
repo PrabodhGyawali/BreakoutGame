709b6d1612b3f50c3771186c5d909da1df51b556
Prabodhcoolkid <prabodh.gyawali2005@gmail.com>
2024-03-26 20:56:29 +0000

    Organized code

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
+import numpy
+
+# Custom Modules
+from assets import *
 
 pygame.init()
-
-# TODO: Adjust size of border/tiles/screen using variables for ease
-x, y = 10, 10
-space = x*0.4
-movex = 0
 ########################## SCREEN ################################
 # Screen_width = (30 * 14) + tilespacing + 40
 screen = pygame.display.set_mode((x*63.2, y*90))
@@ -21,49 +20,37 @@ while running:
     screen.fill("black")
 
     # RENDER YOUR GAME HERE
-    BLUE = (0, 141, 218)
-    player = pygame.draw.rect(screen, BLUE, pygame.Rect(x*2 + movex, y*80, x*3.5, y*1.5))
-
+    pygame.draw.rect(screen, BLUE, player)
     # Game borders 
-    WHITE = (255, 255, 255)
-    wall_left = pygame.draw.rect(screen, WHITE, pygame.Rect(0, 0, x*1, y*90))
-    wall_right = pygame.draw.rect(screen, WHITE, pygame.Rect(x*62.2, 0, x*1, y*90))
+    pygame.draw.rect(screen, WHITE, wall_left)
+    pygame.draw.rect(screen, WHITE, wall_right)
 
-    wall_left = pygame.draw.rect(screen, BLUE, pygame.Rect(0, y*79.5, x*1, y*3))
-    wall_right = pygame.draw.rect(screen, BLUE, pygame.Rect(x*62.2, y*79.5, x*1, y*3))
+    # Game border blue
+    pygame.draw.rect(screen, BLUE, pygame.Rect(0, y*79.5, x*1, y*3))
+    pygame.draw.rect(screen, BLUE, pygame.Rect(x*62.2, y*79.5, x*1, y*3))
 
     # Loop generating tiles
-    RED, ORANGE, GREEN, YELLOW = (237,25,9), (242,133,0), (50,205,50), (255,255,0)
-    COLORS = (RED, ORANGE, GREEN, YELLOW)
-    red_tiles, orange_tiles, green_tiles, yellow_tiles = [], [], [], []
-    tile_array = [red_tiles, orange_tiles, green_tiles, yellow_tiles]
-    # red tile has no spacing
-    for j in range(4):    
-        for k in range(1,3):
-            for i in range(14):
-                tile_array[j].append(pygame.draw.rect(screen, COLORS[j], pygame.Rect(
-                    x*1 + (i*x*4) + i*space,
-                    y*20 + k*x*1.5 + k*space +2*j*x*1.5 + 2*j*space,
-                    x*4, 
-                    y*1.5)))
-                
-    # Game Functionality
+    for i in range(4):
+        for tile in tile_array[i]:
+            pygame.draw.rect(screen, COLORS[i], tile)
+   
+    ######################## Game Functionality #########################
+    # Key Events
     if event.type == pygame.KEYDOWN:
         key=pygame.key.name(event.key)
         if key == "right":
             if player.right < 62.2*x:
-                movex +=5
+                player.move_ip(5, 0)
         if key == "left":
             if player.left > 10:
-                movex -=5 
-    
+                player.move_ip(-5, 0)
+        
+    # Collision detector
+    pygame.draw.rect(screen, WHITE, pong)
+        
 
-        pygame.display.flip()
-        # pygame.time.delay(20)
     # Updates screen
     pygame.display.flip()
-
-
     # limit FPS to 60
     clock.tick(60) 
 
